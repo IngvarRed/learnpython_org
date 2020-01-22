fe864c7c67bdc5bed2da007af812a344ed8df4fb
Igor Bilinskyy <igor.bilinskyy@gmail.com>
2020-01-23 00:05:48 +0200

    chekio_Popular Words

---
+++ chekio_Popular Words.py
@@ -0,0 +1,35 @@
+# Popular Words
+
+'''
+In this mission your task is to determine the popularity of certain words in the text.
+At the input of your function are given 2 arguments: the text and the array of words the popularity of which you need to determine.
+When solving this task pay attention to the following points:
+
+The words should be sought in all registers. This means that if you need to find a word "one" then words like "one", "One", "oNe", "ONE" etc. will do.
+The search words are always indicated in the lowercase.
+If the word wasn’t found even once, it has to be returned in the dictionary with 0 (zero) value.
+
+Input: The text and the search words array.
+Output: The dictionary where the search words are the keys and values are the number of times when those words are occurring in a given text.
+'''
+
+def popular_words(text: str, words: list) -> dict:
+    # your code here
+    dict_word = {}
+    lst_text = text.lower().split()
+    for word in words:
+        dict_word[word]=lst_text.count(word)
+    return dict_word
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(popular_words('''
+When I was One
+I had just begun
+When I was Two
+I was nearly new
+''', ['i', 'was', 'three', 'near']))
+
+
+exit()
