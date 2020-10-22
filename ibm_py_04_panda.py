692b2cbafaa797d174d62053ed20a100f275c91b
Igor Bilinskyy <igor.bilinskyy@gmail.com>
2020-10-22 23:31:33 +0300

    '

---
+++ ibm_py_04_panda.py
@@ -0,0 +1,17 @@
+# Loading data with pandas
+
+
+import pandas as pd
+
+df=pd.DataFrame({'a':[11,21,31],'b':[21,22,23]})
+df.head(3)
+df['a']
+
+
+import pandas as banana
+df=banana.DataFrame({'a':[11,21,31],'b':[21,22,23]})
+df.head()
+
+
+import pandas as pd
+df=pd.DataFrame({'a':[1,2,1],'b':[1,1,1]})
